2e3fdc6c6b7a1288a3894be1e9c579afa7047420
Pee Tankulrat <ptankulrat@gmail.com>
2020-03-23 11:26:52 +0700

    Morgan|Pii Start to refactor score to get the score of the player who win the point

--- tennis-game/TennisGame.cs
+++ tennis-game/TennisGame.cs
@@ -20,10 +20,19 @@ namespace tennis.test
             { 30, 2},
             { 40, 3}
         };
+
+        private Dictionary<Player, int> PlayerScore = new Dictionary<Player, int>{
+            { Player.One, 0},
+            { Player.Two, 0},
+        };
+
         public TennisGame(int score1, int score2)
         {
             this._score1 = ScoreMap[score1];
             this._score2 = ScoreMap[score2];
+
+            PlayerScore[Player.One] = ScoreMap[score1];
+            PlayerScore[Player.Two] = ScoreMap[score2];
         }
 
         public String display()
@@ -40,18 +49,28 @@ namespace tennis.test
 
         public String scores(Player player)
         {
+            PlayerScore[player]++;
             if (player == Player.One)
+            {
                 _score1++;
+                if (PlayerScore[player] == 4 && PlayerScore[Opponent(player)] != 3)
+                    return "Player One Wins";
+            }
             else
+            {
                 _score2++;
-
-            // Check win conditions
-            if (_score1 == 4 && _score2 != 3)
-                return "Player One Wins";
-            else if (_score2 == 4 && _score1 != 3)
-                return "Player Two Wins";
+                if (PlayerScore[player] == 4 && PlayerScore[Opponent(player)] != 3)
+                    return "Player Two Wins";
+            }
 
             return display();
+        }
+
+        private Player Opponent(Player player) {
+            if(player == Player.One)
+            return Player.Two;
+
+            return Player.One;
         }
     }
 }
